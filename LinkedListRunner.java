0c0b2f1d16e34b3eb32bd0fe1ec4cdf34de25601
Richard Bennett <rbenne06@dcs-bbk-ac-uk>
2013-01-14 21:07:25 +0000

    end of class

---
+++ LinkedListRunner.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LinkedListRunner {
+	public static void main(String args[]) {
+		LinkedList<String> myList = new LinkedList<>();;
+		myList.add("aaa\n");
+		myList.add("bbb\n");
+		myList.add("ccc\n");
+		myList.add("ddd\n");
+		myList.add("eee\n");
+		myList.add("fff\n");
+		myList.add("ggg\n");
+		myList.add("hhh\n");
+		myList.add("iii\n");
+
+		ListIterator<String> itr = myList.listIterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		
+		char tmp;
+		while(itr.hasPrevious()) {
+			tmp = itr.toString().charAt(0);
+			if(tmp=='a'||tmp=='e'||tmp=='i'||tmp=='o'||tmp=='u'){
+				itr.remove();
+			}
+		}
+		
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	
+	}
+}
